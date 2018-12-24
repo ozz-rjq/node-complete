29ff48274e45d7da890eb5e8a1071b8b50bfcac1
Taras Tataryn <taras.tataryn.ttm@gmail.com>
2018-12-24 19:51:32 +0200

    Send html response

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const reqListener = function(req, res) {
-  console.log(req);
-  res.end('you have been connected to the server!');
+  console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write("<html>");
+  res.write("<head><title>Render HTML</title></head>");
+  res.write("<body>");
+  res.write("<h1>Welcome to the Server</h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
 }
 
 http.createServer(reqListener).listen(3000);

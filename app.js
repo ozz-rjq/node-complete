6fd17133985681dcf767abb97b0a763fd2da49bc
Taras Tataryn <taras.tataryn.ttm@gmail.com>
2018-12-24 20:05:07 +0200

    Handle different urls

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const http = require('http');
 
 const reqListener = function(req, res) {
-  console.log(req.url, req.method);
+  const url = req.url,
+        method = req.method;
 
   res.setHeader('Content-Type', 'text/html');
 
   res.write("<html>");
   res.write("<head><title>Render HTML</title></head>");
   res.write("<body>");
-  res.write("<h1>Welcome to the Server</h1>");
+  res.write("<h1>Hello from Node.js server</h1>");
+
+  if (url === "/") {
+    res.write("<p>You reached: " + url + " path.</p>");
+  } else if (url === "/message" && method === "POST") {
+    res.write("<p>POST method has been sended</p>");
+  } else {
+    res.write("<form action='/message' method='POST'><input type='text' name='name'><button type='submit'>POST</button></form>");
+  }
+
   res.write("</body>");
   res.write("</html>");
+
   res.end();
 }
 

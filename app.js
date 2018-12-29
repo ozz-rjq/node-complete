cc4aec30add171bb0c5cc7873808e407e88d23d3
Taras Tataryn <taras.tataryn.ttm@gmail.com>
2018-12-29 22:45:18 +0200

    Parse request body using body-parser

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>Add product page</h1>');
+  res.send("<form action='/product' method='POST'><input type='text' name='name'><button type='submit'>Add product</button></form>");
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {

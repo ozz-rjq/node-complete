9469b345659d4492923edf396dd0cfe3aae7e966
Taras Tataryn <taras.tataryn.ttm@gmail.com>
2018-12-30 11:02:55 +0200

    Filter requests route

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>You\'ve entered some bullsh*t, please try again</h1>');
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,4 @@ router.get('/', (req, res, next) => {
   res.send('<h1>Hello From Express.js</h1>');
 });
 
-router.get('**', (req, res, next) => {
-  res.send('<h1>You\'ve enetred somu bullsh*t, please try again</h1>');
-});
-
 module.exports = router;

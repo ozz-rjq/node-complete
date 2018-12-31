3e1b74ad3d907ee6254cbfc301e28914c8868a9c
Taras Tataryn <taras.tataryn.ttm@gmail.com>
2018-12-31 17:48:33 +0200

    Create models

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,18 @@
 const products = [];
 
+const Product = require('./../models/product');
+
 module.exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {path: '/admin/add-product'});
 }
 
 module.exports.postAddProduct = (req, res, next) => {
-  products.push({title: req.body.title});
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 }
 
 module.exports.getAllProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', {products: products, path: '/'});
 }
